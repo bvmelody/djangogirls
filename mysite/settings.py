cf830d7fc0b81c0622a4ae4267428851280be1d3
Brenden Melody <bvmelody@icloud.com>
2026-02-28 15:03:34 -0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%reib8s+%(ohyzz@!qb*!4^lp*bql)21(t$v8#@oi_m^wj$%*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bmelody.pythonanywhere.com']
 
 
 # Application definition
